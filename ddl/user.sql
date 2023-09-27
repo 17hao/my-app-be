a8c7dad5e345c797c727d84a2e9a4ef7cbe35a86
shiqihao <shiqihao@bytedance.com>
2023-09-27 17:52:30 +0800

    update users table ddl

--- ddl/user.sql
+++ ddl/user.sql
@@ -1,9 +1,11 @@
 create table users
 (
-    id        bigint unsigned primary key default 0,
+    id        bigint unsigned default 0,
     name      varchar(255) not null default '',
     age       int          not null default 0,
     city      varchar(255),
     phone_num varchar(255),
-    extra     text
+    extra     text,
+    primary key (id),
+    unique index uniq_phonenum(phone_num)
 );
