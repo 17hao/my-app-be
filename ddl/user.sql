4b683929f2b7c7e85af4b3d6c3f6bcef210959e9
17hao <sqh1107@gmail.com>
2024-09-05 23:56:18 +0800

    chore: update ddl

--- ddl/user.sql
+++ ddl/user.sql
@@ -1,11 +1,11 @@
 create table users
 (
-    id        bigint unsigned default 0,
-    name      varchar(255) not null default '',
-    age       int          not null default 0,
-    city      varchar(255),
-    phone_num varchar(255),
-    extra     text,
+    id         bigint unsigned default 0,
+    name       varchar(255) not null default '',
+    age        int,
+    city       varchar(255),
+    phone_num  varchar(255),
+    is_deleted tinyint(1) not null default 0,
     primary key (id),
     unique index uniq_phonenum(phone_num)
 );
